1b2f585bd7a6e51d757aac00ceadc99ebbdee8b0
wendi <ifyouseewendy@gmail.com>
2013-08-16 18:22:41 +0800

    Remove oh-my-zsh submodules, and check into main repo

---
+++ oh-my-zsh/custom/environment.zsh
@@ -0,0 +1,24 @@
+# zsh
+unsetopt correct_all
+
+HISTFILE=~/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=HISTSIZE
+
+# MacPorts is uninstalled, so no need to add `/opt/local/bin` to $PATH
+# [[ -s "/opt/local/bin" ]] && export PATH=/opt/local/bin:$PATH
+[[ -s "/usr/local/bin" ]] && export PATH=/usr/local/bin:$PATH
+[[ -s "/usr/local/sbin" ]] && export PATH=/usr/local/sbin:$PATH
+
+# I dropped rvm a few days ago, so shell won't add append BUNDLE_BIN path to the
+# PATH when changing directories. And I always use `--binstubs=.binstubs`
+# option for bundler. So it will be great to add `.binstubs` to the PATH.
+# Thank god I won't type `bundle exec` or its alias `be` any more.
+export PATH=.binstubs:$PATH
+
+export EDITOR=vim
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
