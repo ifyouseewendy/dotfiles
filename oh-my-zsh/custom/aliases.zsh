68c20b33269da979bd7756fd50f01c62279af480
wendi <ifyouseewendy@gmail.com>
2013-11-25 11:40:07 +0800

    Update zsh aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
-alias ll='ls -alF'
+alias ll='ls -alhF'
 alias la='ls -A'
 alias l='ls -CF'
 alias grep='grep --color=auto'
@@ -13,28 +13,14 @@ alias vless='/usr/share/vim/vim73/macros/less.sh'
 
 alias h='history'
 alias g='git status'
-function gg() {
-  git commit -v -a -m "$*"
-}
 
-# safely rm
-mkdir -p ~/.Trash
-alias rm=trash
-alias urm=untrash
-alias rl="ls ~/.Trash"
+alias tm='open -a /Applications/TextMate.app'
+alias mvim='open -a /Applications/MacVim.app'
+# alias dman='open dash://{query}'
 
-trash()
-{
-  mv $@ --verbose -f --backup=numbered --target-directory ~/.Trash/
-}
+alias crontab="VIM_CRONTAB=true crontab"
 
-untrash()
-{
-  mv -i --verbose ~/.Trash/$@ ./
-}
+# auto open gz by tar
+alias -s gz='tar -xzvf'
 
-cleartrash()
-{
-  read -p "clear sure?[n]" confirm
-  [ $confirm == 'y' ] || [ $confirm == 'Y' ]  && /usr/bin/rm -rf ~/.Trash/*
-}
+alias pre='qlmanage -p'
