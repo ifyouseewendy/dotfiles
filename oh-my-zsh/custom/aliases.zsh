8faeccd7ab960b485691b9f9909529e8e0f2f351
wendi <ifyouseewendy@gmail.com>
2014-01-18 09:29:23 +0800

    Add pg-start/stop alias

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -24,3 +24,9 @@ alias crontab="VIM_CRONTAB=true crontab"
 alias -s gz='tar -xzvf'
 
 alias pre='qlmanage -p'
+
+alias tailf='tail -f'
+
+alias pg-start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias pg-stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
