8ce30fd49a02a6ad8baadfaa18cdcd5877eb96dc
wendi <ifyouseewendy@gmail.com>
2015-06-04 11:34:48 +0800

    Update zsh to use c-z to switch vim and terminal

--- oh-my-zsh/custom/edit-command-line.zsh
+++ oh-my-zsh/custom/edit-command-line.zsh
@@ -1,3 +1,16 @@
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
+
+# Use Ctrl-z to swtich between vim and command line
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
