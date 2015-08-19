a401264ca9e1d560bdd165e416dda82891f17e39
wendi <ifyouseewendy@gmail.com>
2015-08-19 14:18:45 +0800

    zsh: Fix ping bin path

--- oh-my-zsh/custom/environment.zsh
+++ oh-my-zsh/custom/environment.zsh
@@ -9,6 +9,7 @@ SAVEHIST=HISTSIZE
 # [[ -s "/opt/local/bin" ]] && export PATH=/opt/local/bin:$PATH
 [[ -s "/usr/local/bin" ]] && export PATH=/usr/local/bin:$PATH
 [[ -s "/usr/local/sbin" ]] && export PATH=/usr/local/sbin:$PATH
+[[ -s "/sbin" ]] && export PATH=/sbin:$PATH
 
 # I dropped rvm a few days ago, so shell won't add append BUNDLE_BIN path to the
 # PATH when changing directories. And I always use `--binstubs=.binstubs`
