2768e516b2eb5c410ae681cca569d0f3d2fda61e
wendi <ifyouseewendy@gmail.com>
2016-04-13 12:06:20 +0800

    rbenv.plugin: Fix searching path and shims

--- oh-my-zsh/plugins/rbenv/rbenv.plugin.zsh
+++ oh-my-zsh/plugins/rbenv/rbenv.plugin.zsh
@@ -4,19 +4,19 @@ _homebrew-installed() {
 
 FOUND_RBENV=0
 rbenvdirs=("$HOME/.rbenv" "/usr/local/rbenv" "/opt/rbenv" "/usr/local/opt/rbenv")
-if _homebrew-installed && rbenv_homebrew_path=$(brew --prefix rbenv 2>/dev/null); then
-    rbenvdirs=($rbenv_homebrew_path "${rbenvdirs[@]}")
-    unset rbenv_homebrew_path
-fi
+# if _homebrew-installed && rbenv_homebrew_path=$(brew --prefix rbenv 2>/dev/null); then
+#     rbenvdirs=($rbenv_homebrew_path "${rbenvdirs[@]}")
+#     unset rbenv_homebrew_path
+# fi
 
 for rbenvdir in "${rbenvdirs[@]}" ; do
-  if [ -d $rbenvdir/bin -a $FOUND_RBENV -eq 0 ] ; then
+  if [ -d $rbenvdir/shims -a $FOUND_RBENV -eq 0 ] ; then
     FOUND_RBENV=1
     if [[ $RBENV_ROOT = '' ]]; then
       RBENV_ROOT=$rbenvdir
     fi
     export RBENV_ROOT
-    export PATH=${rbenvdir}/bin:$PATH
+    export PATH=${rbenvdir}/shims:$PATH
     eval "$(rbenv init --no-rehash - zsh)"
 
     alias rubies="rbenv versions"
