855735bfa4319f8d8850a6a1eb6918262eadefec
Di Wen <di.wen@shopify.com>
2022-02-10 16:27:48 -0800

    Debug remove source etc profile

--- install.sh
+++ install.sh
@@ -28,26 +28,27 @@ else
   exit 1
 fi
 
-### Install utils
-
+# ### Install utils
+#
 UTILS=(direnv ripgrep neovim fzf fd tig diff-so-fancy chezmoi)
 
 for i in "${UTILS[@]}"
 do
+  echo "--> installing $i"
   $(echo "$INSTALLER $i")
 done
-
-### Apply dotfiles
-
-chezmoi init https://github.com/ifyouseewendy/dotfiles.git --branch=chezmoi
-chezmoi apply
-
-### Post hooks
-# neovim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
-# vim-plug
-nvim --headless +PlugInstall +qa
-
-# tpm
-$HOME/.tmux/plugins/tpm/bin/install_plugins
+#
+# ### Apply dotfiles
+#
+# chezmoi init https://github.com/ifyouseewendy/dotfiles.git --branch=chezmoi
+# chezmoi apply
+#
+# ### Post hooks
+# # neovim
+# ln -s ~/.vimrc ~/.config/nvim/init.vim
+#
+# # vim-plug
+# nvim --headless +PlugInstall +qa
+#
+# # tpm
+# $HOME/.tmux/plugins/tpm/bin/install_plugins
