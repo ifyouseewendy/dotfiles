07fe4081948497422d34599c72604bebde6b1339
Di Wen <di@cointracker.com>
2023-04-04 14:37:26 -0700

    nvim: Tweak neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,5 @@
 return {
+	-- https://github.com/nvim-neo-tree/neo-tree.nvim/
 	"nvim-neo-tree/neo-tree.nvim",
 	event = { "VimEnter" },
 	branch = "v2.x",
@@ -17,6 +18,8 @@ return {
 						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
 					},
 					["o"] = "open",
+					["H"] = "prev_source",
+					["L"] = "next_source",
 				},
 			},
 		})
