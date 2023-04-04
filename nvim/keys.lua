306a5e30406d2604dad05410273b95caac1ec975
Di Wen <di@cointracker.com>
2023-04-03 20:52:13 -0700

    nvim: Add comment for vim-expand-region

--- nvim/keys.lua
+++ nvim/keys.lua
@@ -1,1 +1,2 @@
-vim.keymap.set('n', '<F1>', ':Neotree toggle=true<cr>')
+-- vim.keymap.set ignores noreamp for clarity. Use remap instead.
+vim.keymap.set('n', '<F1>', ':Neotree toggle=true<cr>', { remap = False })
--- nvim/lua/plugins/vim-expand-region.lua
+++ nvim/lua/plugins/vim-expand-region.lua
@@ -1,3 +1,5 @@
 return {
+  -- "vv" is not compatible with nvim yet according to the issue https://github.com/terryma/vim-expand-region/issues/23
+  -- use "+" instead
   "terryma/vim-expand-region"
 }
