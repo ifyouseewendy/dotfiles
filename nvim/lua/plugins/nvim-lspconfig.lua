e208d050d459356079f75c9092d0e8dac0b24814
Di Wen <di@cointracker.com>
2023-04-04 12:42:21 -0700

    nvim: Add nvim-cmp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
   config = function()
     -- Setup language servers.
     local lspconfig = require('lspconfig')
@@ -31,6 +34,30 @@ return {
       elseif client.server_capabilities.document_range_formatting then
         buf_set_keymap("n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
       end
+    end
+
+    -- Hook nvim-cmp
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    -- Specify a virtual env for Pyright
+    -- https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-876700701
+    local util = require('lspconfig/util')
+    local path = util.path
+    local function get_python_path(workspace)
+      -- Use activated virtualenv.
+      if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+      end
+
+      -- Find and use virtualenv via poetry in workspace directory.
+      local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+      if match ~= '' then
+        local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+        return path.join(venv, 'bin', 'python')
+      end
+
+      -- Fallback to system Python.
+      return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
     end
 
     -- Set up lspconfig for pyright
