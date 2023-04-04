82432bc239ac00e3a50903ec8835dc74b7b46ffb
Di Wen <di@cointracker.com>
2023-04-04 10:19:01 -0700

    nvim: Remove vim-expand-region
    It's not compatible with nvim. Use treesitter instead

--- nvim/lua/plugins/vim-expand-region.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- "vv" is not compatible with nvim yet according to the issue https://github.com/terryma/vim-expand-region/issues/23
-  -- use "+" instead
-  "terryma/vim-expand-region"
-}
