419ae3a6b8f6387ebe26bf916ff742e40ab546e9
Di Wen <di@cointracker.com>
2023-04-04 12:30:00 -0700

    nvim: Install Mason tools

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,12 +3,25 @@ return {
   lazy = false,
   priority = 900, -- Load before nvim-lspconfig. Default priority 50.
   dependencies = {
-    "williamboman/mason-lspconfig.nvim"
+    "williamboman/mason-lspconfig.nvim",
+    -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+    "WhoIsSethDaniel/mason-tool-installer.nvim"
   },
   config = function()
     require("mason").setup()
-    require("mason-lspconfig").setup {
-      ensure_installed = { "pyright" },
-    }
+    require('mason-tool-installer').setup {
+      -- a list of all tools you want to ensure are installed upon
+      -- start; they should be the names Mason uses for each tool
+      ensure_installed = {
+        -- Python
+        "pyright",
+        "ruff",
+        "black",
+
+        -- Lua
+        "luaformatter"
+      }
+  }
+    require("mason-lspconfig").setup()
   end
 }
