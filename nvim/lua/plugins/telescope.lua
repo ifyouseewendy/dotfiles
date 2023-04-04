07275d278ba18928a0f637ae5190d6453fbfe1c5
Di Wen <di@cointracker.com>
2023-04-04 08:22:21 -0700

    nvim: Add telescope-fzf-native

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,7 +9,15 @@ return {
   -- * <C-Q> send all items to quickfix
   'nvim-telescope/telescope.nvim',
   tag = '0.1.1',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- There is no auto bulding. You have to call build manually.
+    -- In addition, lazy would not automatically build again if the plugin is already installed or not updated,
+    -- so to re-build, delete the folder ~/.local/share/nvim/lazy/
+    -- https://github.com/nvim-telescope/telescope-fzf-native.nvim/issues/96
+    -- :Lazy build telescope-fzf-native.nvim
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = false }
+  },
   config = function()
     local builtin = require('telescope.builtin')
     local theme = require('telescope.themes').get_ivy()
@@ -28,6 +36,24 @@ return {
     --  :nnoremap <Leader>ppp :Telescope git_bcommits theme=ivy<CR>
     --  :nnoremap <Leader>ppp :Telescope git_status theme=ivy<CR>
     --  :nnoremap <Leader>ppp :Telescope commands theme=ivy<CR>
+
+    require('telescope').setup {
+      extensions = {
+        -- telescope-fzf-native.nvim
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                           -- the default case_mode is "smart_case"
+        }
+      }
+    }
+
+    -- telescope-fzf-native.nvim
+    -- To get fzf loaded and working with telescope, you need to call
+    -- load_extension, somewhere after setup function:
+    require('telescope').load_extension('fzf')
   end
 }
 
