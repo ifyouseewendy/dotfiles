6d4ddbe8c9dcfe86f892913818e6a2501efe18dd
Di Wen <di@cointracker.com>
2023-04-04 20:57:02 -0700

    nvim: Configure neo-tree to close when last window

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -11,6 +11,7 @@ return {
 	config = function()
 		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 		require("neo-tree").setup({
+			close_if_last_window = true,
 			window = {
 				mappings = {
 					["x"] = {
