07b8bb47491cecfd2b7fdd31591c18a761edbb9b
Di Wen <di@cointracker.com>
2023-04-04 20:56:54 -0700

    nvim: Add copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+	-- NOT ENABLED YET
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- event = "InsertEnter",
+	-- config = function()
+	-- 	require("copilot").setup({})
+	-- end,
+}
