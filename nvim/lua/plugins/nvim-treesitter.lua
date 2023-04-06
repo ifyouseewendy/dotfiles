9e000e8927a24ff2e90588f19646df6cfb2911b1
Di Wen <di@cointracker.com>
2023-04-06 14:43:52 -0700

    nvim: Enable treesitter for Terraform

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -11,7 +11,7 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "python", "vim", "lua" },
+			ensure_installed = { "python", "vim", "lua", "terraform" },
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
