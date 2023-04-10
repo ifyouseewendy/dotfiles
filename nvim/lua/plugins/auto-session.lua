d17d8e789b030233a65af83ce4bd73d54ff6b228
Di Wen <di@cointracker.com>
2023-04-09 21:16:36 -0700

    nvim: Config tabline to show filename only

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -6,6 +6,6 @@ return {
 			log_level = "error",
 			auto_session_suppress_dirs = { "~/", "~/Workspace", "~/Downloads", "/" },
 		})
-		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+		vim.o.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 	end,
 }
