a129b9a554df2d3215d6eabd920e436ce8378e1e
Di Wen <di@cointracker.com>
2023-04-09 17:00:06 -0700

    nvim: Add copilot-cmp

---
+++ nvim/lua/plugins/copilot-cmp.lua
@@ -0,0 +1,17 @@
+return {
+	"zbirenbaum/copilot-cmp",
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+	},
+	config = function()
+		require("copilot").setup({
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		})
+		require("copilot_cmp").setup({
+			formatters = {
+				insert_text = require("copilot_cmp.format").remove_existing,
+			},
+		})
+	end,
+}
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -36,6 +36,8 @@ return {
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = cmp.config.sources({
+				-- Copilot Source
+				{ name = "copilot", group_index = 2 },
 				{ name = "nvim_lsp" },
 				{ name = "vsnip" }, -- For vsnip users.
 				-- { name = 'luasnip' }, -- For luasnip users.
@@ -61,6 +63,26 @@ return {
 		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+		})
+
+		-- Tab completition configuration by copilot-cmp
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+		end
+		cmp.setup({
+			mapping = {
+				["<Tab>"] = vim.schedule_wrap(function(fallback)
+					if cmp.visible() and has_words_before() then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						fallback()
+					end
+				end),
+			},
 		})
 	end,
 }
