99fb7d1654aff27f97c401ae1adc4725bc502a03
Di Wen <di@cointracker.com>
2023-04-19 17:39:44 -0700

    nvim: Comment on vim-surround

--- nvim/lua/plugins/vim-surround.lua
+++ nvim/lua/plugins/vim-surround.lua
@@ -2,9 +2,10 @@ return {
 	-- https://github.com/tpope/vim-surround
 	--
 	-- cs"' - change surround word from " to '
-	-- css"' - change surround word from " to '
+	-- css"' - change surround line from " to '
 	-- ysiw" - add word surrounding with "
 	-- yss" - add line surrounding with "
+	-- ds{ - delete word surrounding {
 	--
 	"tpope/vim-surround",
 }
