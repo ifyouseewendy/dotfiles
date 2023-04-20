2d902e99ac1ee49f38ede3a4d15563cb63af24d1
Di Wen <di@cointracker.com>
2023-04-19 17:39:14 -0700

    nvim: Fix lualine filename

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -28,7 +28,7 @@ return {
 				lualine_c = {
 					{
 						"filename",
-						path = 3, -- 0: Just the filename
+						path = 1, -- 0: Just the filename
 						-- 1: Relative path
 						-- 2: Absolute path
 						-- 3: Absolute path, with tilde as the home directory
