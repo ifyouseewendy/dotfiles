55359bd78b441261a2c9c14b418d80323bdd533e
Di Wen <di@cointracker.com>
2023-08-02 17:44:15 -0700

    nvim: Pin fidget.nvim to legacy

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -2,6 +2,7 @@ return {
 	-- https://github.com/j-hui/fidget.nvim
 	-- Useful status updates for LSP
 	"j-hui/fidget.nvim",
+	tag = "legacy",
 	config = function()
 		require("fidget").setup({})
 	end,
