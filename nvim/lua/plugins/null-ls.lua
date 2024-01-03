8c37677b9c639ff9c935dc2f84a4af51327fc6d3
Di Wen <di@cointracker.com>
2024-01-03 10:02:48 -0800

    nvim: Enable ruff formatting

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -14,6 +14,7 @@ return {
 				null_ls.builtins.diagnostics.ruff,
 				null_ls.builtins.diagnostics.mypy,
 				null_ls.builtins.formatting.ruff_import,
+				null_ls.builtins.formatting.ruff,
 				null_ls.builtins.formatting.ruff_format,
 				-- Lua
 				-- Use extra_args = { "--indent-type", "Spaces" } to format with spaces
