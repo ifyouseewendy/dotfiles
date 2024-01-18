7caccb3f62daf186c8e60ac78787b133296a9498
Di Wen <di@cointracker.com>
2024-01-17 20:03:11 -0800

    nvim: Enable copilot

--- nvim/lua/plugins/copilot-cmp.lua
+++ nvim/lua/plugins/copilot-cmp.lua
@@ -1,19 +1,18 @@
 return {
-	-- DISABLE IT AS IT GETS REALLY SLOW
-	--
-	-- "zbirenbaum/copilot-cmp",
-	-- dependencies = {
-	-- 	"zbirenbaum/copilot.lua",
-	-- },
-	-- config = function()
-	-- 	require("copilot").setup({
-	-- 		suggestion = { enabled = false },
-	-- 		panel = { enabled = false },
-	-- 	})
-	-- 	require("copilot_cmp").setup({
-	-- 		formatters = {
-	-- 			insert_text = require("copilot_cmp.format").remove_existing,
-	-- 		},
-	-- 	})
-	-- end,
+	-- https://github.com/zbirenbaum/copilot-cmp
+	"zbirenbaum/copilot-cmp",
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+	},
+	config = function()
+		require("copilot").setup({
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		})
+		require("copilot_cmp").setup({
+			formatters = {
+				insert_text = require("copilot_cmp.format").remove_existing,
+			},
+		})
+	end,
 }
